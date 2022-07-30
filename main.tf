56461f14636a4d97de90eda5929d977eb6d347ce
varun <varunmanikoutlo@ip-172-31-18-86.ec2.internal>
2022-07-30 17:51:25 +0000

    adding V4

--- main.tf
+++ main.tf
@@ -79,6 +79,7 @@ resource "aws_instance" "ubuntu" {
       "sudo apt-get update",
       "sudo apt-get install -y openjdk-8-jre",
       "sudo apt-get install -y jenkins",
+      "sudo apt-get install -y docker docker.io",
     ]
   }
 
